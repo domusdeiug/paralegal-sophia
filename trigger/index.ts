70b3cb59bd2eaf5d472c27f42191d26170a757a9
domusdeiug <domusdeiug@gmail.com>
2026-06-04 12:44:12 +0000

    deleted the trigger files, trigger config and the dependencies

--- trigger/index.ts
+++
@@ -1,6 +0,0 @@
-// trigger/index.ts
-// Registers all tasks with Trigger.dev
-
-export { ingestChat } from "./tasks/ingest-chat";
-export { ingestAgent } from "./tasks/ingest-agent";
-export { embedCorpus } from "./tasks/embed-corpus";
